5ca2758e15c0c325c978b06269258fb2353fd0b2
Daniel Poverjuc <daniel.poverjuc@iongroup.com>
2025-11-26 16:07:22 +0200

    added read from keyboard method (console)

--- task1/src/main/java/com/rekka/Main.java
+++ task1/src/main/java/com/rekka/Main.java
@@ -4,45 +4,95 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
 
-        // graf cu 10 noduri (0-9)
+    public static List<List<Integer>> readGraphFromInput() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Numarul de noduri: ");
+        int n = scanner.nextInt();
+
         List<List<Integer>> graph = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < n; i++) {
             graph.add(new ArrayList<>());
         }
 
-        // adaugam muchii (graf neorientat)
-        graph.get(5).add(2);
-        graph.get(5).add(4);
+        System.out.print("Numarul de muchii: ");
+        int m = scanner.nextInt();
 
-        graph.get(2).add(5);
-        graph.get(2).add(7);
-        graph.get(2).add(1);
+        System.out.println("Introdu muchiile (u v) pentru graf neorientat:");
+        for (int i = 0; i < m; i++) {
+            int u = scanner.nextInt();
+            int v = scanner.nextInt();
 
-        graph.get(4).add(5);
-        graph.get(4).add(3);
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
 
-        graph.get(7).add(2);
-        graph.get(7).add(6);
+        return graph;
+    }
 
-        graph.get(1).add(2);
-        graph.get(1).add(8);
 
-        graph.get(3).add(4);
+    public static void main(String[] args) {
+        // graf cu 10 noduri (0-9)
+        List<List<Integer>> graph = new ArrayList<>();
 
-        graph.get(6).add(7);
-        graph.get(6).add(9);
+        boolean readFromKeyboad = true;
+        if (readFromKeyboad) {
+            graph = readGraphFromInput();
+        } else {
 
-        graph.get(8).add(1);
+            for (int i = 0; i < 10; i++) {
+                graph.add(new ArrayList<>());
+            }
 
-        graph.get(9).add(6);
+            // adaugam muchii (graf neorientat)
+            graph.get(5).add(2);
+            graph.get(5).add(4);
+
+            graph.get(2).add(5);
+            graph.get(2).add(7);
+            graph.get(2).add(1);
+
+            graph.get(4).add(5);
+            graph.get(4).add(3);
+
+            graph.get(7).add(2);
+            graph.get(7).add(6);
+
+            graph.get(1).add(2);
+            graph.get(1).add(8);
+
+            graph.get(3).add(4);
+
+            graph.get(6).add(7);
+            graph.get(6).add(9);
+
+            graph.get(8).add(1);
+
+            graph.get(9).add(6);
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nod start BFS: ");
+        int start = scanner.nextInt();
 
         // rulam bfs din nodul 5
-        bfs(5, graph);
+        bfs(start, graph);
     }
+
+    /**
+     * Marchezi nodul de start ca vizitat
+     * Il pui in coada
+     * Cat timp coada nu este goala:
+     * scoti primul nod din coada
+     * il vizitezi (il afisezi)
+     * adaugi in coada toti vecinii nevizitati
+     * Repeti pana coada se goleste.
+     * Ordinea vizitarii este determinata de coada, deci intotdeauna BFS merge pe orizontala inainte sa coboare la urmatorul nivel.
+     * */
 
     // functie bfs
     public static void bfs(int start, List<List<Integer>> graph) {
@@ -50,7 +100,7 @@ public class Main {
         boolean[] visited = new boolean[n];
         Queue<Integer> queue = new LinkedList<>();
 
-        // adaugam varful initial
+        // adaugamm varful initial
         visited[start] = true;
         queue.add(start);
 
